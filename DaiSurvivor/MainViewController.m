ebcb41cce6b27c217a764e25e185be001afff708
DaidoujiChen <daidoujichen@gmail.com>
2015-04-29 17:36:31 +0800

    first commit

---
+++ DaiSurvivor/MainViewController.m
@@ -0,0 +1,21 @@
+//
+//  MainViewController.m
+//  DaiSurvivor
+//
+//  Created by DaidoujiChen on 2015/4/29.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+}
+
+@end
